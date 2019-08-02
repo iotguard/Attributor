7822cb1d626f507a4b8296b5894c0359a69fdf95
iotguard <caowentao@iotguard.cn>
2019-08-02 12:12:32 +0800

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by dianyi on 2019/8/2.
+//  Copyright © 2019 dianyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
