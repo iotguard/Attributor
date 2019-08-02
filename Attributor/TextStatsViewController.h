d52e271aa7b1234c2330655d377be87b56331a98
iotguard <caowentao@iotguard.cn>
2019-08-02 18:09:16 +0800

    lecture 6

---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,16 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by dianyi on 2019/8/2.
+//  Copyright © 2019 dianyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
+
